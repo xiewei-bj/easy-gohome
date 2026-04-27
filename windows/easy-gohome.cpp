5f09db0ed4f663312403d1032a10cded168535fb
xiewei <xiewei@raisecom.com>
2026-04-27 16:48:43 +0800

    更新win工程

---
+++ windows/easy-gohome.cpp
@@ -0,0 +1,262 @@
+﻿// WindowsProject1.cpp : 定义应用程序的入口点。
+//
+
+
+#include <WebView2.h>
+#include <wrl.h>
+#include <string>
+#include <wrl/event.h> 
+#include <shlwapi.h>
+#include "framework.h"
+#include "easy-gohome.h"
+#pragma comment(lib, "shlwapi.lib")
+#define MAX_LOADSTRING 100
+using namespace Microsoft::WRL;
+// 全局变量:
+HINSTANCE hInst;                                // 当前实例
+WCHAR szTitle[MAX_LOADSTRING];                  // 标题栏文本
+WCHAR szWindowClass[MAX_LOADSTRING];            // 主窗口类名
+// WebView2 全局
+ComPtr<ICoreWebView2Controller> m_webviewController;
+ComPtr<ICoreWebView2> m_webview;
+
+
+// 此代码模块中包含的函数的前向声明:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+void InitWebView2(HWND hWnd);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: 在此处放置代码。
+
+    // 初始化全局字符串
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_EASYGOHOME, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // 执行应用程序初始化:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_EASYGOHOME));
+
+    MSG msg;
+
+    // 主消息循环:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  函数: MyRegisterClass()
+//
+//  目标: 注册窗口类。
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDC_EASYGOHOME));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName = nullptr;// MAKEINTRESOURCEW(IDC_WINDOWSPROJECT1);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   函数: InitInstance(HINSTANCE, int)
+//
+//   目标: 保存实例句柄并创建主窗口
+//
+//   注释:
+//
+//        在此函数中，我们在全局变量中保存实例句柄并
+//        创建和显示主程序窗口。
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // 将实例句柄存储在全局变量中
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle,  WS_MINIMIZEBOX | WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU,
+      100, 100, 360, 640, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+   InitWebView2(hWnd);
+   return TRUE;
+}
+
+//
+//  函数: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  目标: 处理主窗口的消息。
+//
+//  WM_COMMAND  - 处理应用程序菜单
+//  WM_PAINT    - 绘制主窗口
+//  WM_DESTROY  - 发送退出消息并返回
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // 分析菜单选择:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: 在此处添加使用 hdc 的任何绘图代码...
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// “关于”框的消息处理程序。
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
+
+
+
+void InitWebView2(HWND hWnd)
+{
+    CreateCoreWebView2EnvironmentWithOptions(nullptr, nullptr, nullptr,
+        Callback<ICoreWebView2CreateCoreWebView2EnvironmentCompletedHandler>(
+            [hWnd](HRESULT result, ICoreWebView2Environment* env) -> HRESULT {
+
+                env->CreateCoreWebView2Controller(hWnd, Callback<ICoreWebView2CreateCoreWebView2ControllerCompletedHandler>(
+                    [hWnd](HRESULT result, ICoreWebView2Controller* controller) -> HRESULT {
+
+                        if (controller == nullptr) return E_FAIL;
+                        m_webviewController = controller;
+                        m_webviewController->get_CoreWebView2(&m_webview);
+
+                        // 充满窗口
+                        RECT rc;
+                        GetClientRect(hWnd, &rc);
+                        m_webviewController->put_Bounds(rc);
+
+                        // ==============================================
+                        // 注入 JS 接口：webMessage.connect / disconnect
+                        // ==============================================
+                        m_webview->AddScriptToExecuteOnDocumentCreated(LR"(
+                            window.webMessage = {
+                                connect: function(json) {
+                                    window.chrome.webview.postMessage({ type:'connect', data:json });
+                                    return true;
+                                },
+                                disconnect: function() {
+                                    window.chrome.webview.postMessage({ type:'close' });
+                                }
+                            };
+                        )", nullptr);
+
+                        // ==============================================
+                        // 接收 JS 消息
+                        // ==============================================
+                        m_webview->add_WebMessageReceived(
+                            Callback<ICoreWebView2WebMessageReceivedEventHandler>(
+                                [](ICoreWebView2*, ICoreWebView2WebMessageReceivedEventArgs* args) -> HRESULT {
+
+                                    LPWSTR message = NULL;
+                                    args->get_WebMessageAsJson(&message);
+                                    // 关闭窗口
+                                    if (wcsstr(message, L"close")) {
+                                        MessageBoxW(NULL, message, L"close", MB_OK);
+                                    }
+
+                                    // 连接按钮
+                                    if (wcsstr(message, L"connect")) {
+                                        MessageBoxW(NULL, message, L"connect", MB_OK);
+                                    }
+                                    return S_OK;
+                                }).Get(),NULL);
+
+                        // ====================== 关键路径 ======================
+                        WCHAR szPath[MAX_PATH] = { 0 };
+                        GetModuleFileName(NULL, szPath, MAX_PATH);  // 获取 exe 完整路径
+                        wcsrchr(szPath, L'\\')[1] = 0;              // 截取到文件夹
+
+                        std::wstring url = L"file:///";
+                        url += szPath;
+                        url += L"index.html";                       // 拼接网页
+                        // ======================================================
+                        m_webview->Navigate(url.c_str());
+                    }).Get());
+                return S_OK;
+            }).Get());
+}
